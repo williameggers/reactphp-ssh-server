c3cced950ef8a4b3cd27ec102436002a5d935225
William Eggers <williameggers@users.noreply.github.com>
2025-07-05 19:34:30 +1000

    Public key authentication example (#3)
    * Public key authentication example.
    * Additional tests.

--- tests/Unit/AuthenticationTest.php
+++ tests/Unit/AuthenticationTest.php
@@ -24,12 +24,17 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+use phpseclib3\Crypt\Common\PrivateKey;
+use phpseclib3\Crypt\PublicKeyLoader;
 use phpseclib3\Net\SSH2;
 
 beforeEach(function () {
     // Use a random available port for testing
     $this->port = rand(49152, 65535);
     $this->host = '127.0.0.1';
+    $this->rsaTestKeys = generateTestKeyPair('rsa');
+    $this->ed25519TestKeys = generateTestKeyPair('ed25519');
+    $this->extraEd25519TestKeys = generateTestKeyPair('ed25519');
 
     // TODO: Refactor so we can test with a basic Server class instance
     $this->serverScript = sys_get_temp_dir() . '/reactphp_ssh_auth_test_server_' . uniqid() . '.php';
@@ -39,11 +44,21 @@ beforeEach(function () {
         <<<PHP
 <?php
 require '{$autoloadPath}';
+use phpseclib3\\Crypt\\PublicKeyLoader;
 use React\\Promise\\Deferred;
 use WilliamEggers\\React\\SSH\\Channel;
 use WilliamEggers\\React\\SSH\\Connection;
 use WilliamEggers\\React\\SSH\\Loggers\\ConsoleLogger;
 use WilliamEggers\\React\\SSH\\Server;
+
+\$authorizedPublicKeys = [
+    'test' => [
+        '{$this->ed25519TestKeys['public']}',
+    ],
+    'test2' => [
+        '{$this->rsaTestKeys['public']}',
+    ]
+];
 
 \$server = new Server('tcp://{$this->host}:{$this->port}');
 \$server->setLogger(new ConsoleLogger());
@@ -51,18 +66,56 @@ use WilliamEggers\\React\\SSH\\Server;
 \$server->enableAuthentication();
 \$server->info('Listening on {$this->host}:{$this->port}');
 
-\$server->on('connection', function (Connection \$connection) {
+\$server->on('connection', function (Connection \$connection) use (\$authorizedPublicKeys) {
     \$connection->info('Connection accepted from ' . \$connection->getRemoteAddress());
-    \$connection->on('authenticate', function (string \$username, string \$method, array \$credentials, Deferred \$authenticated) {
+    \$connection->on('authenticate', function (string \$username, string \$method, array \$credentials, Deferred \$authenticated) use (\$authorizedPublicKeys) {
         \$isAuthenticated = false;
-        \$password = \$credentials[0] ?? null;
 
-        if (
-            'test' === \$username
-            && 'password' === \$method
-            && 'abc123' === \$password
-        ) {
-            \$isAuthenticated = true;
+        switch (\$method) {
+            case 'password':
+                \$password = \$credentials[0] ?? null;
+
+                if (
+                    'test' === \$username && 'abc123' === \$password
+                ) {
+                    \$isAuthenticated = true;
+                }
+
+                break;
+
+            case 'publickey':
+                \$publicKeyString = \$credentials[0] ?? null;
+
+                try {
+                    /**
+                     * Create a public key object from the public key string, then extract its fingerprint.
+                     *
+                     * @var string \$clientFingerprint
+                     */
+                    \$publicKeyFingerprint = PublicKeyLoader::loadPublicKey(\$publicKeyString)->getFingerprint('sha256');
+
+                    /**
+                     * Precompute fingerprints for each trusted key for comparison purposes.
+                     *
+                     * @var array<string> \$authorizedFingerprints
+                     */
+                    \$authorizedFingerprints = array_map(
+                        fn (\$keyString) => PublicKeyLoader::loadPublicKey(\$keyString)->getFingerprint('sha256'),
+                        \$authorizedPublicKeys[\$username] ?? []
+                    );
+
+                    foreach (\$authorizedFingerprints ?? [] as \$authorizedFingerprint) {
+                        if (hash_equals(\$authorizedFingerprint, \$publicKeyFingerprint)) {
+                            \$isAuthenticated = true;
+
+                            break;
+                        }
+                    }
+                } catch (Throwable \$e) {
+                    // Invalid key format — log or ignore as needed
+                }
+
+                break;
         }
 
         \$authenticated->resolve(\$isAuthenticated);
@@ -99,7 +152,20 @@ afterEach(function () {
 
     // Clean up process if it's still running
     if (isset($this->process) && is_resource($this->process)) {
-        proc_terminate($this->process, SIGKILL);
+        $status = proc_get_status($this->process);
+        if (true == $status['running']) { // process ran too long, kill it
+            // get the parent pid of the process we want to kill
+            $ppid = $status['pid'];
+            // use ps to get all the children of this process, and kill them
+            $pids = preg_split('/\s+/', shell_exec('ps -o pid --no-heading --ppid ' . $ppid) ?? '');
+            foreach ($pids as $pid) {
+                if (is_numeric($pid)) {
+                    posix_kill((int) $pid, 9); // 9 is the SIGKILL signal
+                }
+            }
+
+            proc_close($this->process);
+        }
     }
 });
 
@@ -127,6 +193,66 @@ test('connection using phpseclib ssh client with invalid credentials', function 
     $client = new SSH2($this->host, $this->port);
     $client->setTimeout(1);
     expect($client->login('test', 'incorrectpassword'))->toBeFalse();
+    expect($client->isAuthenticated())->toBeFalse();
+    expect($client->getBannerMessage())->toBe('PEST AuthenticationTest');
+
+    $client->disconnect();
+});
+
+test('connection using phpseclib ssh client with private ed25519 key', function () {
+    // Arrange
+    ['process' => $this->process, 'pid' => $this->pid, 'pipes' => $this->pipes] = start_server_and_wait_for_listening($this->serverScript, $this->host, $this->port);
+
+    $client = new SSH2($this->host, $this->port);
+    $privateKey = PublicKeyLoader::load($this->ed25519TestKeys['private']);
+    $this->assertInstanceOf(PrivateKey::class, $privateKey);
+    $client->setTimeout(1);
+    expect($client->login('test', $privateKey))->toBeTrue();
+    expect($client->isAuthenticated())->toBeTrue();
+    expect($client->getBannerMessage())->toBe('PEST AuthenticationTest');
+
+    $client->disconnect();
+});
+
+test('connection using phpseclib ssh client with incorrect private key', function () {
+    // Arrange
+    ['process' => $this->process, 'pid' => $this->pid, 'pipes' => $this->pipes] = start_server_and_wait_for_listening($this->serverScript, $this->host, $this->port);
+
+    $client = new SSH2($this->host, $this->port);
+    $privateKey = PublicKeyLoader::load($this->rsaTestKeys['private']);
+    $this->assertInstanceOf(PrivateKey::class, $privateKey);
+    $client->setTimeout(1);
+    expect($client->login('test', $privateKey))->toBeFalse();
+    expect($client->isAuthenticated())->toBeFalse();
+    expect($client->getBannerMessage())->toBe('PEST AuthenticationTest');
+
+    $client->disconnect();
+});
+
+test('connection using phpseclib ssh client with private rsa key', function () {
+    // Arrange
+    ['process' => $this->process, 'pid' => $this->pid, 'pipes' => $this->pipes] = start_server_and_wait_for_listening($this->serverScript, $this->host, $this->port);
+
+    $client = new SSH2($this->host, $this->port);
+    $privateKey = PublicKeyLoader::load($this->rsaTestKeys['private']);
+    $this->assertInstanceOf(PrivateKey::class, $privateKey);
+    $client->setTimeout(1);
+    expect($client->login('test2', $privateKey))->toBeTrue();
+    expect($client->isAuthenticated())->toBeTrue();
+    expect($client->getBannerMessage())->toBe('PEST AuthenticationTest');
+
+    $client->disconnect();
+});
+
+test('connection using phpseclib ssh client with non-authorized private ed25519 key', function () {
+    // Arrange
+    ['process' => $this->process, 'pid' => $this->pid, 'pipes' => $this->pipes] = start_server_and_wait_for_listening($this->serverScript, $this->host, $this->port);
+
+    $client = new SSH2($this->host, $this->port);
+    $privateKey = PublicKeyLoader::load($this->extraEd25519TestKeys['private']);
+    $this->assertInstanceOf(PrivateKey::class, $privateKey);
+    $client->setTimeout(1);
+    expect($client->login('test', $privateKey))->toBeFalse();
     expect($client->isAuthenticated())->toBeFalse();
     expect($client->getBannerMessage())->toBe('PEST AuthenticationTest');
 
